73daea53f8ee4a172cf842c9ca3f7f2fc214a0ac
Kacper <56606499+KaoticKas@users.noreply.github.com>
2023-04-08 18:13:03 +0100

    updating branch

--- Model/main.py
+++ Model/main.py
@@ -6,13 +6,10 @@ from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
 
-    data = tf.keras.utils.image_dataset_from_directory("img")
-    data_iterator = data.as_numpy_iterator()
-    batch = data_iterator.next()
-
-    fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-    for idx, img in enumerate(batch[0][:4]):
-        ax[idx].imshow(img.astype(int))
-        ax[idx].title.set_text(batch[1][idx])
-    
-    plt.show()
+    tf.compat.v1.disable_eager_execution()
+    with tf.device('/gpu:0'):
+        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+        c = tf.matmul(a, b)
+    with tf.compat.v1.Session() as sess:
+        print (sess.run(c))
