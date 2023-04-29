e7d593ded6a96f88ca2004871bef8b2e1072178b
Kacper <56606499+KaoticKas@users.noreply.github.com>
2023-04-29 22:01:16 +0100

    updating code

--- frontend/flaskserver.py
+++ frontend/flaskserver.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 from tensorflow.keras.models import load_model
-from flask import Flask , render_template  , request , send_file
+from flask import Flask , render_template  , request
 from tensorflow.keras.preprocessing.image import load_img , img_to_array
 #improting the required libraries for the controller to work and be able to use the models to classify
 
@@ -90,6 +90,7 @@ def help():
 
 @app.route('/result', methods =["GET","POST"])
 def result():
+    error_msg = ""
     #target_img = os.path.join(os.getcwd() , 'static/images')
     target_img = (r"C:\Users\Kacper\Desktop\github\OneFish-Disseration-Project\frontend\static\images")
     file = request.files['file']
@@ -98,29 +99,34 @@ def result():
         file.save(os.path.join(target_img , unique_name))
         img_path = os.path.join(target_img , unique_name)
         img = unique_name
-    #Loads the file name into a static/images folder to be used for the prediction 
-    if request.referrer.endswith('/cnnModel'):
-        model = models['CNN Model']
-        img_size = 256 # set image size to required size
-    elif request.referrer.endswith('/transferModel'):
-        model = models['Transfer Model']
-        img_size = 224
-    elif request.referrer.endswith('/rcnnModel'):
-        model = models['RCNN Model']
-        img_size = 255
+    #Loads the file name into a static/images folder to be used for the prediction
+        if request.referrer.endswith('/cnnModel'):
+            model = models['CNN Model']
+            img_size = 256 # set image size to required size
+        elif request.referrer.endswith('/transferModel'):
+            model = models['Transfer Model']
+            img_size = 224
+        elif request.referrer.endswith('/rcnnModel'):
+            model = models['RCNN Model']
+            img_size = 255
+        else:
+            model = models['CNN Model']
+            img_size = 256
     # if else statement to determin which model to load 
-    class_result , prob_result = predict(img_path , model, img_size)
-    predictions = {
-        "class1":class_result[0],
-        "class2":class_result[1],
-        "class3":class_result[2],
-        "prob1": prob_result[0],
-        "prob2": prob_result[1],
-        "prob3": prob_result[2],
-        }
-    #produces a list of predictions
-    return render_template('result.html', img = img, predictions = predictions)
-
+        class_result , prob_result = predict(img_path, model, img_size)
+        predictions = {
+            "class1":class_result[0],
+            "class2":class_result[1],
+            "class3":class_result[2],
+            "prob1": prob_result[0],
+            "prob2": prob_result[1],
+            "prob3": prob_result[2],
+            }
+        #produces a list of predictions
+        return render_template('result.html', img = img, predictions = predictions)
+    else:
+        error_msg = "the was no image uploaded or the file type was not supported, Please Choose an Image that is of type PNG, JPG or JFIF"
+        return render_template('index.html', error_msg = error_msg)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000,debug = True)
